dc71cda7a9be8c4884f8796b84989bf4a879d75d
Eduardo López <loama18@gmail.com>
2023-11-16 10:44:37 -0600

    Updated configurations with new project ID

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -14,9 +14,9 @@ export default defineNuxtConfig({
     public: {
       // do not update this, it will be overwritten by the build but it needs to be exactly this here
       // otherwise, update the build script in API (to create new projects)
-      GITHUB_REPO: process.env.NUXT_PUBLIC_GITHUB_REPO,
+      GITHUB_REPO: 'manifest-hq/manifest-project-ubgsqm',
       GITHUB_BRANCH: process.env.NUXT_PUBLIC_GITHUB_BRANCH,
-      PROJECT_ID: process.env.NUXT_PUBLIC_PROJECT_ID
+      PROJECT_ID: 'manifest-project-ubgsqm'
     }
   },
   ssr: false,
